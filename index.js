931d78718b2133df739ece480d1bd8a10eb23e58
yoolha <dbstjwls0247@naver.com>
2024-03-22 10:58:59 +0900

    json swagger 삭제

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // index.js
 import express from 'express';
-import { specs } from './config/swagger.config.js';
-import SwaggerUi from 'swagger-ui-express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -19,8 +17,6 @@ app.use(cors());                            // cors 방식 허용
 app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
-
-app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
 
 app.use('/health', healthRoute);
 
